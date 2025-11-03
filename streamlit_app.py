23cc5de498a38299c8738648353dfa84780d091c
TaharaGibbs <tahara.gibbs@student.mec.cuny.edu>
2025-11-03 11:15:59 -0500

    Updated project emoji streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
   st.metric('GPA', '3.8', '📚')
 with col2:
-  st.metric('Projects', '5', '💻')
+  st.metric('Projects', '5', '💼')
 with col3:
   st.metric('Skills', '10+', '🚀')
 st.write('---')

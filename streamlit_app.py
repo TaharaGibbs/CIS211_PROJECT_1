9d4e736ccbea5cac34b01daf3cc86c4380864b85
TaharaGibbs <tahara.gibbs@student.mec.cuny.edu>
2025-11-03 11:06:20 -0500

    Updated hex color streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 st.markdown('''
                 <style>
                     .main-header{font-size: 42px; font-weight: bold; text-align:center;}
-                    .sub-header {font-size: 24px; text-align:center; color: #9F2B68;}
+                    .sub-header {font-size: 24px; text-align:center; color: #7b62b9;}
                   </style>
                   ''', unsafe_allow_html = True)
 # Sidebar

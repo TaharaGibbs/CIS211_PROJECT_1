beac54486c8bed2055a5e96a381a71865ce94967
TaharaGibbs <tahara.gibbs@student.mec.cuny.edu>
2025-12-01 14:43:38 -0500

    Updated columns streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,3 +127,28 @@ elif page == '🔨 Skills':
       'SQL' : 50,
   'Technical Writing' : 40
   }
+
+  for skill, level in skills_data.items():
+    col1, col2 = st.columns([1.3])
+    with col1:
+      st.write(skill)
+    with col2:
+      st.progress(level/100)
+
+  st.subheader('Tools & Technologies')
+
+  col1, col2, col3 = st.columns(3)
+  with col1:
+    st.success('Excel')
+    st.info('Word')
+    st.warning('Access')
+
+  with col2:
+    st.success('Powerpoint')
+    st.info('Google Docs')
+    st.warning('ChatGPT/AI Tools')
+
+  with col3:
+    st.success('Presentations')
+    st.info('Writing')
+    st.warning('Social Media')

6d69efb37a08e62bd45390295d1416a3b58c24b6
TaharaGibbs <tahara.gibbs@student.mec.cuny.edu>
2025-12-17 11:56:53 -0500

    Updated to marketing plan streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,8 +99,8 @@ elif page == '💼 Projects':
         st.image('https://th.bing.com/th/id/R.f1b7e8eab63689611376b0b7f23d4fea?rik=cuYCuq2bGsAPBQ&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f5%2fDigital-Marketing-PNG.png&ehk=1zUcafp%2fggRj2H8Rw%2fXijbJLed2QWcKIpX31ZlhXbKo%3d&risl=&pid=ImgRaw&r=0')
     
     with col2:
-      st.subheader('🧮Business Plan')
-      st.write("A comprehensive document that was created as a means of outlining and tracking a companies' goals, desired achievements, strategies, and projections")
+      st.subheader('🧮Marketing Plan')
+      st.write("A comprehensive document that was created as a means of outlining and tracking a companies' goals, desired achievements, strategies, and projections for their marketing department")
       st.caption('**Technologies:** Python, BeautifulSoup, Streamlit')
 
 
